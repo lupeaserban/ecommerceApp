b433a40b1a3e1712e9f0047828c1d8c8e8b69387
Serban Lupea <lupea.serban@gmail.com>
2018-10-22 18:51:21 +0300

    comeeet

---
+++ src/views/Projects/Projects.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// @material-ui/core
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+// import Store from "@material-ui/icons/Store";
+
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import Table from "components/Table/Table.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+
+import CardBody from "components/Card/CardBody.jsx";
+
+// import { bugs, website, server } from "variables/general.jsx";
+
+// import {
+//   dailySalesChart,
+//   emailsSubscriptionChart,
+//   completedTasksChart
+// } from "variables/charts.jsx";
+
+import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
+
+class Projects extends React.Component {
+  state = {
+    value: 0
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <CardHeader color="primary">
+                <h4 className={classes.cardTitleWhite}>Stats</h4>
+                <p className={classes.cardCategoryWhite}>Details</p>
+              </CardHeader>
+
+              <CardBody>
+                <Table
+                  tableHeaderColor="primary"
+                  tableHead={[
+                    "ID",
+                    "Client",
+                    "Project Name",
+                    "Project Manager",
+                    "Deadline",
+                    "Organizer"
+                  ]}
+                  tableData={[
+                    ["12", "Niger", "Oud-Turnhout", "X", "this year"],
+                    ["23", "Curaçao", "Sinaai-Waas", "Y", "this month"],
+                    ["45", "Netherlands", "Baileux", "Z", "today"]
+                  ]}
+                />
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
+}
+
+Projects.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(dashboardStyle)(Projects);
